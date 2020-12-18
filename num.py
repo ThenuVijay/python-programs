e4911d410dc7a6923d1e59baf83b163233431dd2
ThenuVijay <warunvijay@gmail.com>
2020-12-18 21:17:09 +0530

    Homework Program

---
+++ num.py
@@ -0,0 +1,9 @@
+mark1=int(input("enter your mark1:"))
+mark2=int(input("enter your mark2:"))
+mark3=int(input("enter your mark3:"))
+if mark1>mark2 and mark1>mark3:
+    print("m1 is greater")
+elif mark2>mark3:
+    print("m2 is greater")
+else:
+    print("m3 is greater")
